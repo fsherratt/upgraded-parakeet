97b3152a8925b6a30a1549fdf6666b60d4da1a75
Freddie Sherratt <fsherratt@gmail.com>
2020-04-20 17:37:12 +0100

    Refactored to indicate private and general items code refinement

--- modules/realsense_d435.py
+++ modules/realsense_d435.py
@@ -2,12 +2,14 @@ import pyrealsense2 as rs
 import traceback
 import sys
 import time
-
 import numpy as np
 
-
 class rs_d435:
+    """
+    Declare all the constants, tunable variables are public
+    """
     def __init__(self):
+        # public
         self.depth_width = 640
         self.depth_height = 480
 
@@ -19,97 +21,70 @@ class rs_d435:
         self.min_range = 0.1
         self.max_range = 10
 
-        self.intrin = None
-        self.scale = 1
+        # private
+        self._pipe = None
+
+        self._intrin = None
+        self._scale = 1
 
         self._FOV = (0, 0)
 
-        self.x_deproject_matrix = None
-        self.y_deproject_matrix = None
+        self._x_deproject_matrix = None
+        self._y_deproject_matrix = None
 
+    """
+    with enter method opens D435 connection
+    """
     def __enter__(self):
         self.open_connection()
 
+    """
+    with exit method closes the D435 connection
+    """
     def __exit__(self, exception_type, exception_value, traceback):
         if traceback:
             print(traceback.tb_frame)
 
         self.close_connection()
     
-    # --------------------------------------------------------------------------
-    # openConnection
-    # return void
-    # --------------------------------------------------------------------------
+    """
+    Open Connection to D435 camera
+    """
     def open_connection(self):
-        self.pipe = rs.pipeline()
-
-        self.cfg = rs.config()
-        self.cfg.enable_stream( rs.stream.depth, 
+        cfg = rs.config()
+        cfg.enable_stream(rs.stream.depth, 
                                 self.depth_width, self.depth_height,
                                 rs.format.z16, 
                                 self.framerate )
-        self.cfg.enable_stream(rs.stream.color, 
+        cfg.enable_stream(rs.stream.color, 
                                 self.rgb_width, self.rgb_height, 
                                 rs.format.bgr8, 
                                 self.framerate)
 
-        self.profile = self.pipe.start( self.cfg  )
+        self._pipe = rs.pipeline()
+        self._pipe.start( cfg  )
 
-        self.initialise_deprojection_matrix()
+        self._initialise_deprojection_matrix()
 
         print('rs_d435:D435 Connection Open')
     
-    # --------------------------------------------------------------------------
-    # closeConnection
-    # return void
-    # --------------------------------------------------------------------------
+    """
+    Close connection to D435 camera
+    """
     def close_connection(self):
-        self.pipe.stop()
-        
+        if self._pipe is None:
+            return
+
+        self._pipe.stop()
+        self._pipe = None
+
         print('rs_t265:D435 Connection Closed')
 
-    # --------------------------------------------------------------------------
-    # getIntrinsics
-    # get camera intrinsics
-    # return void
-    # --------------------------------------------------------------------------
-    def get_intrinsics( self ):
-        profile = self.pipe.get_active_profile()
-
-        self.intrin = profile.get_stream( rs.stream.depth ) \
-                        .as_video_stream_profile() \
-                        .get_intrinsics()
-
-        self.scale = profile.get_device() \
-                        .first_depth_sensor() \
-                        .get_depth_scale()
-                        
-        self.FOV = rs.rs2_fov( self.intrin )
-        self.FOV = np.deg2rad( self.FOV )
-
-    # --------------------------------------------------------------------------
-    # initialise_deprojection_matrix
-    # Conversion matrix from detpth to
-    # return void
-    # --------------------------------------------------------------------------
-    def initialise_deprojection_matrix( self ):
-        self.get_intrinsics()
-        
-        # Create deproject row/column vector
-        self.x_deproject_row = (np.arange( self.depth_width ) - self.intrin.ppx) / self.intrin.fx
-        self.y_deproject_col = (np.arange( self.depth_height ) - self.intrin.ppy) / self.intrin.fy
-
-        # Tile across full matrix height/width
-        self.x_deproject_matrix = np.tile( self.x_deproject_row, (self.depth_height, 1) )
-        self.y_deproject_matrix = np.tile( self.y_deproject_col, (self.depth_width, 1) ).transpose()
-
-    # --------------------------------------------------------------------------
-    # getFrame
-    # Retrieve a depth frame with scale metres from camera
-    # return np.float32[width, height]
-    # --------------------------------------------------------------------------
-    def get_frame(self):
-        frames = self.pipe.wait_for_frames()
+    """
+    Retrieve a data from the D435 camera
+    """
+    def get_frame(self) -> tuple:
+        frames = self._pipe.wait_for_frames()
 
         depth_frame = frames.get_depth_frame()
         color_frame = frames.get_color_frame()
@@ -117,55 +92,86 @@ class rs_d435:
         try:
             depth_points = depth_frame.get_data()
             color_image = color_frame.get_data()
-            # Some error about no data in frame
         except AttributeError:
+            # TODO: error about no data in frame
             return None
 
         depth_points = np.asarray(depth_points, dtype=np.float32)
         color_image = np.asanyarray(color_image, dtype=np.uint8)
 
-        depth_points = self.process_depth_frame(depth_points)
+        depth_points = self._process_depth_frame(depth_points)
 
         return (time.time(), depth_points, color_image)
 
-    # --------------------------------------------------------------------------
-    # process_depth_frame
-    # Perform data pre-processing to depth frame
-    # --------------------------------------------------------------------------
-    def process_depth_frame(self, frame):
-        frame = self.scale_depth_frame(frame)
-        frame = self.limit_depth_range(frame)
-        frame = self.deproject_frame(frame)
+    """
+    Returns the FOV of the connected camera
+    """
+    def get_fov(self) -> tuple:
+        return self._FOV
+
+    """
+    Get camera intrinsics
+    """
+    def _get_intrinsics(self):
+        profile = self._pipe.get_active_profile()
+
+        self._intrin = profile.get_stream( rs.stream.depth ) \
+                        .as_video_stream_profile() \
+                        .get_intrinsics()
+
+        self._scale = profile.get_device() \
+                        .first_depth_sensor() \
+                        .get_depth_scale()
+                        
+        self._FOV = rs.rs2_fov(self._intrin)
+        self._FOV = np.deg2rad(self._FOV)
+
+    """
+    Initalise conversion matrix for converting the depth frame to a depojected 3D 
+    cooridinate system
+    """
+    def _initialise_deprojection_matrix(self):
+        self._get_intrinsics()
+        
+        x_deproject_row = (np.arange( self.depth_width ) - self._intrin.ppx) / self._intrin.fx
+        y_deproject_col = (np.arange( self.depth_height ) - self._intrin.ppy) / self._intrin.fy
+
+        self._x_deproject_matrix = np.tile( x_deproject_row, (self.depth_height, 1) )
+        self._y_deproject_matrix = np.tile( y_deproject_col, (self.depth_width, 1) ).transpose()
+
+    """
+    Perform data pre-processing to depth frame
+    """
+    def _process_depth_frame(self, frame:np.array):
+        frame = self._scale_depth_frame(frame)
+        frame = self._limit_depth_range(frame)
+        frame = self._deproject_frame(frame)
 
         return frame
 
-    # --------------------------------------------------------------------------
-    # scale_depth_frame
-    # Scale the depth output to meteres
-    # --------------------------------------------------------------------------
-    def scale_depth_frame(self, frame):
-        return frame * self.scale
+    """
+    Scale the depth output to meteres
+    """
+    def _scale_depth_frame(self, frame:np.array):
+        return frame * self._scale
 
-    # --------------------------------------------------------------------------
-    # limit_depth_range
-    # Limit the maximum/minimum range of the depth camera
-    # --------------------------------------------------------------------------
-    def limit_depth_range(self, frame):
+    """
+    Limit the maximum/minimum range of the depth camera
+    """
+    def _limit_depth_range(self, frame:np.array):
         frame[ frame > self.max_range ] = np.nan
         frame[ frame < self.min_range ] = np.nan
         return frame
 
-    # --------------------------------------------------------------------------
-    # deproject_frame
-    # Conversion depth frame to 3D local coordiate system in meters
-    # return [[x,y,z]] coordinates of depth pixels
-    # --------------------------------------------------------------------------
-    def deproject_frame( self, frame ):       
+    """
+    Converts from depth frame to 3D local FED coordiate system
+    """
+    def _deproject_frame(self, frame:np.array) -> list:       
         Z = frame
-        X = np.multiply( frame, self.x_deproject_matrix )
-        Y = np.multiply( frame, self.y_deproject_matrix )
+        X = np.multiply( frame, self._x_deproject_matrix )
+        Y = np.multiply( frame, self._y_deproject_matrix )
 
-        return [X, Y, Z]
+        return [Z, X, Y] # Output as FRD coordinates
 
 if __name__ == "__main__":
     import cv2
@@ -176,10 +182,10 @@ if __name__ == "__main__":
         while True:
             frame = d435Obj.get_frame()
 
-            depth = frame[1][2]
-            depth = cv2.applyColorMap(cv2.convertScaleAbs(depth, alpha=100), cv2.COLORMAP_JET)
+            depth = frame[1][0]
+            depth = cv2.applyColorMap(cv2.convertScaleAbs(depth, alpha=50), cv2.COLORMAP_JET)
 
-            # depth = np.nan_to_num(depth, nan=1)
+            np.nan_to_num(depth, nan=0)
             cv2.imshow('depth_frame', depth)
             cv2.imshow('color_frame', frame[2])
             cv2.waitKey(1)
--- modules/realsense_t265.py
+++ modules/realsense_t265.py
@@ -5,41 +5,62 @@ import time
 import pyrealsense2 as rs
 
 class rs_t265:
+    """
+    Initialise variables for T265 camera
+    """
     def __init__(self):
-        # Setup variables
-        self.pipe = None
-        self.cfg = None
+        # Private
+        self._pipe = None
 
+    """
+    with __enter__ method opens a connected to the T265 camera
+    """
     def __enter__(self):
         self.open_connection()
 
+    """
+    with __exit__ method closes the connection to the T265 camera
+    """
     def __exit__(self, exception_type, exception_value, traceback):
         if traceback:
             print(traceback.tb_frame)
 
         self.close_connection()
 
+    """
+    Open a connected to the T265 camera
+    """
     def open_connection(self):
-        self.pipe = rs.pipeline()
+        cfg = rs.config()
+        cfg.enable_stream(rs.stream.pose)
 
-        self.cfg = rs.config()
-        self.cfg.enable_stream(rs.stream.pose)
-
-        self.pipe.start(self.cfg)
+        self._pipe = rs.pipeline()
+        self._pipe.start(cfg)
         print('rs_t265:T265 Connection Open')
 
+    """
+    Close connected to the T265 camera
+    """
     def close_connection(self):
-        self.pipe.stop()
+        if self._pipe is None:
+            return
+
+        self._pipe.stop()
+        self._pipe = None
+
         print('rs_t265:T265 Connection Closed')
 
+    """
+    Retrieve a data from the T265 camera
+    """
     def get_frame(self) -> tuple:
-        frames = self.pipe.wait_for_frames()
+        frames = self._pipe.wait_for_frames()
         pose = frames.get_pose_frame()
 
         try:
             data = pose.get_pose_data()
         except AttributeError:
-            # Some error about no data in frame
+            # TODO: error about no data in frame
             return None
 
         pos = [data.translation.x, 
@@ -53,11 +74,14 @@ class rs_t265:
 
         conf = data.tracker_confidence
 
-        quat = self.convert_coordinate_frame(quat)
+        quat = self._convert_coordinate_frame(quat)
 
         return (time.time(), pos, quat, conf)
 
-    def convert_coordinate_frame(self, quat) -> list:
+    """
+    TODO: Add method to convert to FED coordinates
+    """
+    def _convert_coordinate_frame(self, quat) -> list:
         return quat
 
 if __name__ == "__main__":   
