b07588fbe423843d48632eaf4f744d24326c9656
Alex John Powell <alexjohnpowell93@gmail.com>
2020-04-26 21:15:57 +0100

    Default for proxy setup

---
+++ tests/test_udp_proxy.py
@@ -0,0 +1,16 @@
+from context import modules
+import unittest
+
+from modules.udp_proxy import udp_proxy
+DEFAULT_IP = '127.0.0.1'
+DEFAULT_PORT = 5005
+
+class TestTemplate( TestCase ):
+    def setUp(self):
+        self.udp_proxy = udp_proxy( DEFAULT_IP, DEFAULT_PORT )
+    
+    def tearDown(self):
+        pass
+
+    def test_example_assert(self):
+        self.assertEqual(test.testFunc(), 10)
