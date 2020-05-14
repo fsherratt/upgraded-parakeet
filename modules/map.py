51f12afb3a6bdc04bc564d09332a4e658ab54215
Freddie Sherratt <fsherratt@gmail.com>
2020-05-14 11:33:45 +0100

    Moved initialisation lines to separate methods

--- modules/map.py
+++ modules/map.py
@@ -1,51 +1,82 @@
 import numpy as np
-from scipy import interpolate
+from scipy.interpolate import RegularGridInterpolator as RGI
 from scipy import io
 
-from modules import load_config
+from modules import load_config, async_message
 
 class Map:
     def __init__(self, config_file='conf/map.yaml'):
         self.conf = load_config.from_file(config_file)
+        self.interp_func = None
 
-        self.x_bins = np.linspace(self.conf.map.size.x_min,
-                                  self.conf.map.size.x_max,
-                                  self.conf.map.resolution.x_divisions)
-        self.y_bins = np.linspace(self.conf.map.size.y_min,
-                                  self.conf.map.size.y_max,
-                                  self.conf.map.resolution.y_divisions)
-        self.z_bins = np.linspace(self.conf.map.size.z_min,
-                                  self.conf.map.size.z_max,
-                                  self.conf.map.resolution.z_divisions)
+        self.bins = self.initialise_bins(self.conf)
+        self.grid = self.initialise_grid(self.conf)
 
-        self.grid = np.zeros((self.conf.map.resolution.x_divisions,
-                              self.conf.map.resolution.y_divisions,
-                              self.conf.map.resolution.z_divisions),
-                             dtype=np.int16)
+        self._initialise_interp_func()
 
-        bins = (self.x_bins, self.y_bins, self.z_bins)
+        self.new_map_data = async_message.AsyncMessageCallback()
 
-        self.interp_func = interpolate.RegularGridInterpolator(bins, self.grid,
-                                                               method='linear',
-                                                               bounds_error=False,
-                                                               fill_value=np.nan)
-
-    def update_map(self, points, count):
+    @staticmethod
+    def initialise_bins(map_conf):
         """
-        Adds count at (N,3) coordiante points array to grid
+        Initialise grid divisions
         """
-        np.add.at(self.grid, points, count)
+        x_bins = np.linspace(map_conf.map.size.x_min,
+                             map_conf.map.size.x_max,
+                             map_conf.map.resolution.x_divisions)
+        y_bins = np.linspace(map_conf.map.size.y_min,
+                             map_conf.map.size.y_max,
+                             map_conf.map.resolution.y_divisions)
+        z_bins = np.linspace(map_conf.map.size.z_min,
+                             map_conf.map.size.z_max,
+                             map_conf.map.resolution.z_divisions)
 
-        self.interp_func.values = self.grid
+        return (x_bins, y_bins, z_bins)
 
-    def query_map(self, query_points):
+    @staticmethod
+    def initialise_grid(map_conf):
         """
-        Returns occupancy value for each cooridnate provided
+        Setup grid system
         """
-        return self.interp_func(query_points)
+        grid = np.zeros((map_conf.map.resolution.x_divisions,
+                         map_conf.map.resolution.y_divisions,
+                         map_conf.map.resolution.z_divisions),
+                        dtype=np.int16)
+
+        return grid
+
+    def add_map_data_callback(self, data):
+        """
+        Callback for incoming map data to add to grid
+        """
+        self.new_map_data.queue_message(data)
 
     def save_to_matlab(self, filename):
         """
         Save the grid to a matlab object
         """
         io.savemat(filename, mdict=dict(map=self.grid), do_compression=False)
+
+    def _initialise_interp_func(self):
+        """
+        """
+        self.interp_func = RGI(self.bins, self.grid,
+                               method='linear',
+                               bounds_error=False,
+                               fill_value=np.nan)
+
+    def _update_map(self):
+        """
+        Adds count at (N,3) coordiante points array to grid
+        """
+        new_data = self.new_map_data.wait_for_message()
+
+        np.add.at(self.grid, new_data.voxels, new_data.count)
+
+        self.interp_func.values = self.grid
+
+    def _query_map(self, query_points):
+        """
+        Returns occupancy value for each cooridnate provided
+        """
+        return self.interp_func(query_points)
