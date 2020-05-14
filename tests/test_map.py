eeda7cb2aee9c56f24c54ca2f28b02a0ca50da50
Freddie Sherratt <fsherratt@gmail.com>
2020-05-14 11:36:19 +0100

    Created map unit test file

---
+++ tests/test_map.py
@@ -0,0 +1,11 @@
+from unittest import TestCase, mock
+
+from context import modules
+from modules.map import Map
+
+class TestMap(TestCase):
+    def setUp(self):
+        self.map = Map()
+
+    def test(self):
+        pass
